f21337f3b554bf3399d89a78cec113b343220afb
Michael Nutt <michael@nuttnet.net>
2019-01-03 12:05:14 -0500

    support ssl

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const cors = require('@koa/cors');
 const Camera = require('./camera');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 const port = process.env.PORT || 3059;
 
@@ -17,13 +20,33 @@ const port = process.env.PORT || 3059;
   app.use(cors());
 
   app.use(async ctx => {
+    if (ctx.path === '/cert.der') {
+      ctx.body = fs.readFileSync('server.cert');
+      ctx.type = 'application/x-x509-ca-cert';
+      return;
+    }
     ctx.assert(ctx.path === '/take', 404, 'Unknown path');
 
     ctx.body = await camera.takePicture();
     ctx.type = 'image/jpeg';
   });
 
-  await app.listen(port);
+  let server;
+  if(process.env.HTTPS) {
+    server = https.createServer({
+      key: fs.readFileSync(process.env.SSL_KEY),
+      cert: fs.readFileSync(process.env.SSL_CERT)
+    }, app.callback());
+
+    // huge improvement on connection time
+    server.on("connection", function (socket) {
+      socket.setNoDelay(true);
+    });
+  } else {
+    server = http.createServer(app.callback());
+  }
+
+  await server.listen(port);
 
   console.log(`Listening on port ${port}`);
 })().catch(e => {
